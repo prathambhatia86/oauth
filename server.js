a7c9ac7a5d7af527977c0279baa84d30b396cd66
prathambhatia86 <85384335+prathambhatia86@users.noreply.github.com>
2023-07-14 01:00:14 +0530

    installed passport and strategy for auth and implemented dotenv for security

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express=require("express");
 const app=express();
 const path=require("path");
 const port=8000;
+require("dotenv").config();
+const config={
+    CLIENT_ID:process.env.CLIENT_ID,
+    CLIENT_SECRET:process.env.CLIENT_SECRET
+}
 app.use(express.static(path.join(__dirname,'public')));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','index.html'));
+})
+app.get('/secret',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','secret.html'));
 })
 app.listen(8000,()=>{
     console.log('connected to port succcessfully!');
