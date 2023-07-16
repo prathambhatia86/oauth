217f06f24efa71576aea522ba15db21583bf163e
prathambhatia86 <85384335+prathambhatia86@users.noreply.github.com>
2023-07-16 23:45:30 +0530

    completed crude oauth flow with client side sessions

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const https=require("https");
 const passport=require("passport");
 const fs=require("fs");
 const {Strategy}=require("passport-google-oauth20");
+const cookieSession=require("cookie-session");
 const port=8000;
 require("dotenv").config();
 const config={
@@ -20,27 +21,54 @@ function verifyCallback(accessToken,refreshToken,profile,done){
     console.log('google profile ',profile);
     done(null,profile);
 }
+app.use(cookieSession({
+    name:'session',
+    maxAge:24*60*60*1000,
+    keys:['this is a cookie']
+}))
 passport.use(new Strategy(AUTH_OPTIONS,verifyCallback))
+passport.serializeUser((user,done)=>{
+   
+    done(null,user);
+});
+passport.deserializeUser((obj,done)=>{
+    done(null,obj);
+});
 app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname,'public')))
+function checkedLoggedIn(req,res,next){
+    const isLoggedIn=(req.isAuthenticated()&&req.user);
+    if(!isLoggedIn)
+    {
+        return res.status(401).json({
+            error:'login first',
+        });
+    }
+    next();
+}
 app.get('/auth/google',passport.authenticate('google',{
     scope:['email'],
 }))
 app.get('/auth/google/callback', passport.authenticate('google',{
     failureRedirect:'/failure',
     successRedirect:'/',
-    session:false,
+    session:true,
 }),(req,res)=>{
     console.log('callback called by google');
 });
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','index.html'));
 })
-app.get('/secret',(req,res)=>{
+app.get('/secret',checkedLoggedIn,(req,res)=>{
     res.sendFile(path.join(__dirname,'public','secret.html'));
 })
 app.get('/failure',(req,res)=>{
     return res.send('failed to authenticate');
+})
+app.get('/auth/logout',(req,res)=>{
+    req.logOut();
+    return res.redirect('/');
 })
 const server = https.createServer({
     key: fs.readFileSync('key.pem'),
